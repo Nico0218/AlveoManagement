78d420c9c21b61b90b6d646b70fb1693d06412ce
Nico <nicolaas.willem.coetzee7@gmail.com>
2020-09-05 15:55:01 +0200

    Add some sample data and a sample save

--- AlveoEnergy/src/app/components/gantt/gantt.component.ts
+++ AlveoEnergy/src/app/components/gantt/gantt.component.ts
@@ -56,7 +56,47 @@ export class GanttComponent implements OnInit, AfterViewInit {
 
 		gantt.gantt.config.xml_date = '%Y-%m-%d %H:%i';
 
+		var tasks = {
+			data: [
+				{
+					id: 1,
+					text: "Project #1",
+					start_date: "2020-09-04",
+					duration: 2,
+					color: "red",
+					end_date: "2020-09-06",
+					progress: 0,
+					parent: 0
+				},
+				{
+					id: 2,
+					text: "Task #1",
+					start_date: "2020-09-04",
+					duration: 1,
+					color: "blue",
+					parent: 1,
+					end_date: "2020-09-05",
+					progress: 0
+				},
+				{
+					id: 3,
+					text: "Task #2",
+					start_date: "2020-09-05",
+					duration: 1,
+					color: "blue",
+					parent: 1,
+					end_date: "2020-09-06",
+					progress: 0
+				}
+			],
+			links:[
+				{ id:1, source:1, target:2, type:"1"},                       //link's id = 1 
+				{ id:2, source:2, target:3, type:"0"}                       //link's id = 2 
+			]
+		};
+
 		gantt.gantt.init(this.ganttContainer.nativeElement);
+		gantt.gantt.parse(tasks)
 
 		const dp = gantt.gantt.createDataProcessor({
 			task: {
@@ -78,5 +118,13 @@ export class GanttComponent implements OnInit, AfterViewInit {
 			.then(([data, links]) => {
 				gantt.gantt.parse({ data, links });
 			});
+	}
+
+	public Save() {
+		var data = [];
+		gantt.gantt.eachTask(ii => {
+			data.push(ii);
+		});
+		console.log(JSON.stringify(data))
 	}
 }
