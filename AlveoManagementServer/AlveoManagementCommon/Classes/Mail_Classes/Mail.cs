888640262dabe08c4ea8696005ed1e9f9a942012
Tinus88 <tspangenberg@hotmail.com>
2020-09-16 08:26:18 +0200

    Added mailkit to backend for email services

---
+++ AlveoManagementServer/AlveoManagementCommon/Classes/Mail_Classes/Mail.cs
@@ -0,0 +1,13 @@
+﻿using AlveoManagementCommon.Interfaces;
+using System.Net.Mail;
+
+namespace AlveoManagementCommon.Classes
+{
+    public class Mail : IMail
+    {
+        public MailAddress From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsBodyHtml { get; set; }
+    }
+}
---
+++ AlveoManagementServer/AlveoManagementCommon/Interfaces/Mail_Interfaces/IMail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace AlveoManagementCommon.Interfaces
+{
+    public interface IMail
+    {
+        public MailAddress From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsBodyHtml { get; set; }
+    }
+}
